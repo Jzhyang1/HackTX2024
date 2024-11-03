2fcf4914d3aa15e55905f984e046eb646398fc3d
jzhyang1 <jzhyang1@gmail.com>
2024-11-03 13:28:27 -0600

    made the webpage boot from root

--- HackTX24/src/lib/MainScreen.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import ChatWindow from "./main_components/ChatWindow.svelte";
-  import PlayerWindow from "./main_components/PlayerWindow.svelte";
-</script>
-
-<div>
-  <ChatWindow></ChatWindow>
-  <PlayerWindow></PlayerWindow>
-</div>
